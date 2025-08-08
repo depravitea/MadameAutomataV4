0f2868178b0b527e2b1e6526994ca090aaee08e8
depravitea <runadarling.ttv@gmail.com>
2025-08-08 11:57:41 -0400

    Add files via upload

---
+++ madame-automata-v4/src/commands/user-limits.ts
@@ -0,0 +1,4 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { prisma } from '../lib/db.js';
+export const data = new SlashCommandBuilder().setName('user-limits').setDescription('View user limits').addUserOption(o=>o.setName('user').setDescription('User'));
+export async function execute(i:any){ const u=i.options.getUser('user')||i.user; const c=await prisma.consent.findFirst({ where:{ guildId:i.guildId!, userId:u.id } }); await i.reply({ content: c?.limits || 'No limits recorded.' }); }
